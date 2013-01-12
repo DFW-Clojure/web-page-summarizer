ff2f37a48ed1a1ae080300e8585b8ec289130ece
Alex Robbins <alexander.j.robbins@gmail.com>
2013-01-12 10:33:32 -0600

    Flushed out logic for home page display.

--- src/wps/controller.clj
+++ src/wps/controller.clj
@@ -4,4 +4,4 @@
 (defn home-page
   "Display the home page with the request form."
   [request]
-  (t/base "Web Page Summarizer" "<p>INPUT HERE</p>"))
+  (t/home-page "http://clojure.org/"))
--- src/wps/templates.clj
+++ src/wps/templates.clj
@@ -1,8 +1,19 @@
 (ns wps.templates
   (:require [net.cgrand.enlive-html :as html]))
 
+(html/defsnippet homepage-content "templates/base.html"
+  [:div.home-page-content :> :*]
+  [initial-value]
+  [[:input (html/attr= :type "text")]] (html/set-attr "value" initial-value))
+
 (html/deftemplate base "templates/base.html"
   [title content]
   [:title] (html/content title)
   [:body [:h1 html/first-child]] (html/content title)
-  [:body] (html/append content))
+  [:div.content] (html/content content))
+
+(defn home-page
+  "Render the home page."
+  [initial-value]
+  (base "Web Page Summarizer"
+        (homepage-content initial-value)))
